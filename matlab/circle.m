a45149025699b88e50ac447f146c1c41c9bd9cc3
Shahin <sgharak1@jhu.edu>
2020-07-07 14:26:45 -0400

    updated with interpolation for circle2

--- matlab/circle.m
+++ matlab/circle.m
@@ -1,52 +1,86 @@
 clc; close all;
-vid_filename = 'D:\200629_test_45fps_noRecording.avi';
-videoReader  = VideoReader(vid_filename);
-% videoReader.NumFrame
-% 1683
-c=zeros(1683,4);
+vid_filename = 'D:\domeLatencyMeasurement\200704_latencyTest\200704_latencyTest_45fps_noRecording_video.avi';
+videoReader  = VideoReader(vid_filename)
+videoWriter = VideoWriter(insertBefore(vid_filename,".","_tracked"));
+num_frames = videoReader.NumFrame; % 1683
+frame_rate = videoReader.FrameRate;
+videoWriter.FrameRate = frame_rate;
+videoWriter.Quality = 100;
+c=zeros(num_frames,4);
+open(videoWriter);
+h = figure;
 tic
-for frame_no = 1:1683%videoReader.NumFrames
-% Read image (i.e. a frame) from a video
-I = read(videoReader,frame_no);
-%hold off
-%ax = imshow(I);
-%hold on
-
-%% Projected image
-% ref: https://www.mathworks.com/matlabcentral/answers/495720-how-to-fit-an-ellipse-to-an-image-in-matlab#answer_405863
-xmin = 0;
-ymin = 450;
-I2 = imcrop(I,[xmin ymin 680 400]);
-
-% Binarize
-Igray = rgb2gray(I2);
-BW = imbinarize(Igray);
-
-% Extract the maximum area
-BW = imclearborder(BW,8);
-BW = bwareafilt(BW,1);
-
-% Calculate centroid, orientation and major/minor axis length of the ellipse
-s = regionprops('table', BW,{'Centroid','Orientation','MajorAxisLength','MinorAxisLength'});
-x_proj = s.Centroid(1)+xmin;
-y_proj = s.Centroid(2)+ymin;
-
-%% crown
-[centers, radii, metric] = imfindcircles(I,[15 30]);
-x_crown = centers(1);
-y_crown = centers(2);
-
-%plot(x_proj, y_proj, 'r+', x_crown, y_crown, 'b+', 'MarkerSize', 15, 'LineWidth', 2);
-if mod(frame_no,100)==0
-    disp(frame_no);
-    toc
+for frame_no = 1:num_frames%videoReader.NumFrames
+    % Read image (i.e. a frame) from a video
+    I = read(videoReader,frame_no);
+    
+    hold off
+    ax = imshow(I);
+    hold on
+    
+    %% Projected image
+    % ref: https://www.mathworks.com/matlabcentral/answers/495720-how-to-fit-an-ellipse-to-an-image-in-matlab#answer_405863
+    xmin = 0;
+    ymin = 450;
+    I2 = imcrop(I,[xmin ymin 900 400]);
+    
+    % Binarize
+    Igray = rgb2gray(I2);
+    BW = imbinarize(Igray);
+    
+    % Extract the maximum area
+    BW = imclearborder(BW,8);
+    BW = bwareafilt(BW,1);
+    
+    % Calculate centroid, orientation and major/minor axis length of the ellipse
+    s = regionprops('table', BW,{'Centroid','Orientation','MajorAxisLength','MinorAxisLength'});
+    x_proj = s.Centroid(1)+xmin;
+    y_proj = s.Centroid(2)+ymin;
+    
+    %% crown
+    [centers, radii, metric] = imfindcircles(I,[11 20]);
+    
+    % picking one cirle
+    if size(centers,1)>1
+        fprintf('check frame %d\n', frame_no)
+        if abs(centers(2,1)-centers(1,1))<100
+            if centers(2,1)>centers(1,1)
+                centers = centers(2,:);
+            else
+                centers = centers(1,:);
+            end
+        else
+            if centers(2,1)>centers(1,1)
+                centers = centers(1,:);
+            else
+                centers = centers(2,:);
+            end
+        end
+    end
+    
+    if isempty(centers)
+        centers = [nan nan];
+    end
+    x_crown = centers(1);
+    y_crown = centers(2);
+    
+    plot(x_proj, y_proj, 'r+', 'MarkerSize', 20, 'LineWidth', 2);
+    plot(x_crown, y_crown, 'b+', 'MarkerSize', 10, 'LineWidth', 2);
+    
+    if mod(frame_no,100)==0
+        disp(frame_no);
+        toc
+    end
+    
+    frame = getframe(gcf);
+    writeVideo(videoWriter,frame);
+    pause(1/frame_rate);
+    c(frame_no,:) = [x_proj, y_proj, x_crown, y_crown];
 end
-%pause(0.03);
 
-c(frame_no,:) = [x_proj, y_proj, x_crown, y_crown];
-end
+close(videoWriter)
 
 k=1:length(c);
-t=(k-1)/videoReader.FrameRate;
-fr = videoReader.FrameRate;
-save('200629_test_45fps_noRecording.mat','c','t','fr')
+t=(k-1)/frame_rate;
+
+save(strrep(vid_filename,'avi','mat'),'c','t','frame_rate')
--- matlab/circle2.m
+++ matlab/circle2.m
@@ -1,16 +1,21 @@
-clc; close all;
-load('200629_test_45fps_noRecording.mat');
+clc; 
+close all;
+load('D:\domeLatencyMeasurement\200704_latencyTest\200704_latencyTest_45fps_yesRecording_video.mat');
 
-x1 = c(:,1);
+x1 = c(t<15,1);
 y1 = c(:,2);
-x2 = c(:,3);
+x2 = c(t<15,3);
 y2 = c(:,4);
+t =t(t<15);
 
+%% plot raw data
+figure(1)
 subplot(2,1,1)
 plot(t,x1,'r.')
 subplot(2,1,2)
 plot(t,x2,'b.')
 
+%% plot normalized data
 figure(2)
 x1n = normalize(x1);
 x2n = -1*normalize(x2);
@@ -18,11 +23,56 @@ x1m = x1n - mean(x1n(t<4));
 x2m = x2n - mean(x2n(t<4));
 plot(t,x1m,'r',t,x2m,'b')
 
-
+%% cross correlation
 figure(3)
 [c,lags] = xcorr(x1m,x2m);
-plot(lags,c)
+stem(lags,c)
+% figure(4)
+% plot(lags,c)
 [argvalue, argmax] = max(c);
-lags(argmax)/fr
+lags(argmax)/frame_rate
+x_fit = round(linspace(argmax-4,argmax+4,9));
+y_fit = c(x_fit);
 
+p = polyfit(x_fit,y_fit,2);
+x_fit2 = linspace(argmax-4,argmax+4,900);
+y_fit2 = polyval(p,x_fit2);
+
+[argvalue, argmax] = max(y_fit2);
+lags2 = interp1(x_fit,lags(x_fit),x_fit2(argmax));
+lags2/frame_rate
+
+figure(4)
+plot(x_fit,y_fit,'ob',x_fit2,y_fit2,'.r')
+
+figure(2)
 % 0.139752 sec (for 200629)
+      
+% 0.1320 200704_latencyTest_30fps_noRecording_video [C]         0.1357      
+% 0.1320 200704_latencyTest_45fps_noRecording_video * ---       0.1310
+% 0.1242 200704_latencyTest_60fps_noRecording_video *           0.1230
+% 0.1320 200704_latencyTest_75fps_noRecording_video [C]         0.1289
+% 0.1320 200704_latencyTest_90fps_noRecording_video [C]         0.1302
+% 0.1398 200704_latencyTest_30fps_yesRecording_video [C]        0.1401
+% 0.1242 200704_latencyTest_45fps_yesRecording_video [C]        0.1256
+% 0.1320 200704_latencyTest_60fps_yesRecording_video [C]        0.1293
+% 0.1242 200704_latencyTest_75fps_yesRecording_video [C][F]     0.1223
+% 0.1320 200704_latencyTest_90fps_yesRecording_video [C][F]     0.1333
+
+% 0<t<15
+% === no  ===== yes (+-0.0078)
+% 0.1320        0.1398
+% 0.1242        0.1242
+% 0.1242        0.1242
+% 0.1242        0.1165
+% 0.1320        0.1320
+
+% 0<t<15 (interpolation)
+% === no  ===== yes (+-0.0078)
+% 0.1355        0.1400
+% 0.1280        0.1205
+% 0.1236        0.1242
+% 0.1210        0.1165
+% 0.1301        0.1333
+
+
