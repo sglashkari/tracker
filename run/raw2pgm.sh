8b521d6b4bb9f07bf35217a4f47ef85d148ab943
Shahin <sgharak1@jhu.edu>
2021-07-24 16:51:06 -0400

    raw2pgm: bash script file to make pgm files from raw files. It also accepts a directory as an argument

---
+++ run/raw2pgm.sh
@@ -0,0 +1,16 @@
+if [[ "$1" != "" ]];
+then
+	DIR="$1"
+else
+	echo "No arguments provided, current directory is selected."
+	DIR=$(pwd)
+fi
+echo $DIR
+raw_files=$(ls $DIR | grep "raw")
+non_raw_file=$(ls $DIR | grep "jpg")
+file_info=$(identify "${DIR}/${non_raw_file}")
+size=$(echo ${file_info} | cut -d " " -f 3)
+echo "Size = $size"
+for f in $raw_files; do
+	ffmpeg -f rawvideo -r 1 -s $size -pix_fmt gray -i "${DIR}/$f" "${DIR}/${f%.raw}.pgm"
+done
