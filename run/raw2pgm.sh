a8c2e47d734af1db2929ab29fc76ec6e26d304be
Shahin <sgharak1@jhu.edu>
2021-07-24 15:58:08 -0400

    raw2pgm: minor correction

--- run/raw2pgm.sh
+++ run/raw2pgm.sh
@@ -6,10 +6,10 @@ else
 	echo "No arguments provided, current directory is selected."
 	DIR=$(pwd)
 fi
-files="$DIR/*.{jpg,pgm}"
+files="${DIR}/*.{jpg,pgm}"
 file_info=$(identify ${files[1]})
 size=$(echo ${file_info} | cut -d " " -f 3)
 echo "Size = $size"
-for f in "$DIR/*.raw"; do
+for f in "${DIR}/*.raw"; do
 	ffmpeg -f rawvideo -r 1 -s $size -pix_fmt gray -i "$f" "${f%.raw}.pgm"
 done
