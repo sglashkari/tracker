f53b015ad913e917e42309e7f210c975b751f611
Shahin <sgharak1@jhu.edu>
2021-07-24 15:27:45 -0400

    raw2pgm: bash script is modified to get a directory address as the input argument

--- run/raw2pgm.sh
+++ run/raw2pgm.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-files=(*.{jpg,pgm})
-non_raw_file=$(echo "${files[1]}")
-file_info=$(identify ${non_raw_file})
+if [[ "$1" != "" ]];
+then
+	DIR="$1"
+else
+	echo "No arguments provided, current directory is selected."
+	DIR=$(pwd)
+fi
+files="$DIR*.{jpg,pgm}"
+file_info=$(identify ${files[1]})
 size=$(echo ${file_info} | cut -d " " -f 3)
 echo "Size = $size"
-for f in *.raw; do
+for f in "$DIR*.raw"; do
 	ffmpeg -f rawvideo -r 1 -s $size -pix_fmt gray -i "$f" "${f%.raw}.pgm"
 done
